f4221a6fca0661c0dbff2128d9bd6574a5fa5b7a
Natasha <natusik0007@yandex.ru>
2019-11-25 16:33:22 +0300

    Fix methods

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
 class Field {
   constructor() {
     this.arrShips = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
-    this.matrix = [];
+    this.battleshipGrid = [];
     for (let i = 0; i < 12; i++) {
-      this.matrix.push([]);
+      this.battleshipGrid.push([]);
       for (let n = 0; n < 12; n++) {
-        this.cell = {
+          var cell = {
           isShip: false,
           isHit: false,
-          isKilled: false
+          isKilled: false,
+          shipCells: []
         };
-        this.matrix[i].push(this.cell);
+        this.battleshipGrid[i].push(cell);
         this.arr = [];
       }
     }
-    this.firstCoordinate = [];
     this.arrForChecking = [];
   }
 
   init() {
     while (this.arrShips.length > 0) {
-      this.makeShip();
+      this.generateShip();
     }
     this.render();
   }
 
-  makeRandomNumberFirstCell() {
-    this.firstCoordinate = [];
+  getRandomFirstCell() {
     let firstcellShipX = Math.floor(1 + Math.random() * 10);
     let firstcellShipY = Math.floor(1 + Math.random() * 10);
-
-    if (this.matrix[firstcellShipY][firstcellShipX].isShip === true) {
-      this.makeRandomNumberFirstCell();
-      return;
-    }
-    this.firstCoordinate.push(firstcellShipX, firstcellShipY);
+    return [firstcellShipX, firstcellShipY];
   }
 
   chooseOrientationNumber() {
     return Math.floor(Math.random() * 2);
   }
 
-  checkShip(firstcellShipX, firstcellShipY, orientation) {
+  canPutShip(firstcellShipX, firstcellShipY, orientation) {
     let isAllowedDrow = true;
     this.arr = [];
     for (let k = 0; k < this.arrShips[0]; k++) {
@@ -49,7 +43,7 @@ class Field {
       let i = firstcellShipY;
       for (let a = i - 1; a < i + 2; a++) {
         for (let b = n - 1; b < n + 2; b++) {
-          if (this.matrix[b][a].isShip === true) {
+          if (this.battleshipGrid[b][a].isShip === true) {
             isAllowedDrow = false;
           }
         }
@@ -62,50 +56,58 @@ class Field {
         firstcellShipY = firstcellShipY + 1;
       }
     }
-    if (isAllowedDrow) {
-      this.drowShip(orientation, this.arr);
-      this.arr = [];
-    } else {
-      this.arr = [];
-      this.makeShip();
-    }
+    // if (isAllowedDrow) {
+    //   this.putShip(orientation, this.arr);
+    //   this.arr = [];
+    // } else {
+    //   this.arr = [];
+    //   this.generateShip();
+    // }
+    return isAllowedDrow;
   }
 
-  makeShip() {
-    this.makeRandomNumberFirstCell();
-    let firstcellShipX = this.firstCoordinate[0];
-    let firstcellShipY = this.firstCoordinate[1];
+  generateShip() {
+    let randomCell = this.getRandomFirstCell();
+    let firstcellShipX = randomCell[0];
+    let firstcellShipY = randomCell[1];
     let orientation = this.chooseOrientationNumber();
 
     if (orientation === 0) {
       if (firstcellShipX + this.arrShips[0] > 11) {
-        this.makeShip();
+        this.generateShip();
         return;
       }
     }
     if (orientation === 1) {
       if (firstcellShipY + this.arrShips[0] > 11) {
-        this.makeShip();
+        this.generateShip();
         return;
       }
     }
-    this.checkShip(firstcellShipX, firstcellShipY, orientation);
+    let isAllowedDrow = this.canPutShip(firstcellShipX, firstcellShipY, orientation);
+      if (isAllowedDrow) {
+        this.putShip(orientation, firstcellShipX, firstcellShipY);
+        this.arr = [];
+      } else {
+        this.arr = [];
+        this.generateShip();
+      }
   }
 
-  drowShip(orientation) {
+  putShip(orientation, firstcellShipX, firstcellShipY) {
     if (this.arr.length !== 0) {
       this.arrForChecking.push(this.arr);
       this.arr = [];
     }
-    let firstcellShipX = this.firstCoordinate[0];
-    let firstcellShipY = this.firstCoordinate[1];
+    // let firstcellShipX = this.firstCoordinate[0];
+    // let firstcellShipY = this.firstCoordinate[1];
     for (let k = 0; k < this.arrShips[0]; k++) {
 
       for (let i = 1; i < 12; i++) {
         for (let n = 1; n < 12; n++) {
           if (firstcellShipX === i) {
             if (firstcellShipY === n) {
-              this.matrix[firstcellShipX][firstcellShipY].isShip = true;
+              this.battleshipGrid[firstcellShipX][firstcellShipY].isShip = true;
             }
           }
         }
@@ -134,36 +136,36 @@ class Field {
       smallContainer.className = "small-container";
       document.querySelector(".container").appendChild(smallContainer);
       for (let n = 1; n < 11; n++) {
-        if (this.matrix[i][n].isShip) {
-          if (!this.matrix[i][n].isHit) {
-            this.matrix[i][n] = document.createElement("div");
-            this.matrix[i][n].className = "ship";
-            this.matrix[i][n].isShip = true;
-            this.matrix[i][n].isHit = false;
-            smallContainer.appendChild(this.matrix[i][n]);
-            this.matrix[i][n].addEventListener("click", () => this.shoot(i, n))
+        if (this.battleshipGrid[i][n].isShip) {
+          if (!this.battleshipGrid[i][n].isHit) {
+            this.battleshipGrid[i][n] = document.createElement("div");
+            this.battleshipGrid[i][n].className = "ship";
+            this.battleshipGrid[i][n].isShip = true;
+            this.battleshipGrid[i][n].isHit = false;
+            smallContainer.appendChild(this.battleshipGrid[i][n]);
+            this.battleshipGrid[i][n].addEventListener("click", () => this.shoot(i, n))
           } else {
-            this.matrix[i][n] = document.createElement("div");
-            this.matrix[i][n].className = "hit";
-            this.matrix[i][n].isShip = true;
-            this.matrix[i][n].isHit = true;
-            smallContainer.appendChild(this.matrix[i][n]);
+            this.battleshipGrid[i][n] = document.createElement("div");
+            this.battleshipGrid[i][n].className = "hit";
+            this.battleshipGrid[i][n].isShip = true;
+            this.battleshipGrid[i][n].isHit = true;
+            smallContainer.appendChild(this.battleshipGrid[i][n]);
           }
         } else {
-          if (this.matrix[i][n].isHit) {
-            this.matrix[i][n] = document.createElement("div");
-            this.matrix[i][n].className = "miss";
-            this.matrix[i][n].isShip = false;
-            this.matrix[i][n].isHit = true;
-            smallContainer.appendChild(this.matrix[i][n]);
+          if (this.battleshipGrid[i][n].isHit) {
+            this.battleshipGrid[i][n] = document.createElement("div");
+            this.battleshipGrid[i][n].className = "miss";
+            this.battleshipGrid[i][n].isShip = false;
+            this.battleshipGrid[i][n].isHit = true;
+            smallContainer.appendChild(this.battleshipGrid[i][n]);
           }
           else {
-            this.matrix[i][n] = document.createElement("div");
-            this.matrix[i][n].className = "sea";
-            this.matrix[i][n].isShip = false;
-            this.matrix[i][n].isHit = false;
-            smallContainer.appendChild(this.matrix[i][n]);
-            this.matrix[i][n].addEventListener("click", () => this.shoot(i, n))
+            this.battleshipGrid[i][n] = document.createElement("div");
+            this.battleshipGrid[i][n].className = "sea";
+            this.battleshipGrid[i][n].isShip = false;
+            this.battleshipGrid[i][n].isHit = false;
+            smallContainer.appendChild(this.battleshipGrid[i][n]);
+            this.battleshipGrid[i][n].addEventListener("click", () => this.shoot(i, n))
           }
         }
       }
@@ -172,8 +174,8 @@ class Field {
 
 
   shoot(i, n) {
-    this.matrix[i][n].isHit = true;
-    if (this.matrix[i][n].isShip) {
+    this.battleshipGrid[i][n].isHit = true;
+    if (this.battleshipGrid[i][n].isShip) {
       for (let b=0; b<this.arrForChecking.length; b++) {
         for (let a=0; a<this.arrForChecking[b].length; a++) {
           if (this.arrForChecking[b][a][0][0] === i && this.arrForChecking[b][a][0][1] === n) {
